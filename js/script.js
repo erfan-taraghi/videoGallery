9467289e17a699ccba3e1ea3f992574e5cb7f091
erfan taraghi <taraghikonerfan@gmail.com>
2022-08-23 19:21:54 +0430

    added project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const videoPlayer = document.querySelector('#videoPlayer');
+const myVideo = document.querySelector('#myVideo');
+
+
+function stopPlayer(){
+    videoPlayer.style.display='none';
+}
+
+function playVideo(file){
+    myVideo.src = file
+    videoPlayer.style.display = 'block';
+}
+
